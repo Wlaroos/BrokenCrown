9ae8f66e7cd099d58f82a136fb4fc297872e9545
Mikey <mikeybess27@gmail.com>
2024-09-07 17:50:31 -0500

    Alternating Shoot Location

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -2,35 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.Serialization;
 
 public class PlayerWeapon : MonoBehaviour
 {
     public event Action Fired = delegate {};
 
-    [SerializeField] private GameObject bulletRef;
+    [SerializeField] private GameObject _bulletRef;
+    
+    [SerializeField] private Transform _aimTransform;
+    [SerializeField] private Transform _shootTransform1;
+    [SerializeField] private Transform _shootTransform2;
 
-    private Vector3 gunEndPointPosition;
-
-    [SerializeField] private Transform shootTransform;
-
-    private Vector3 mousePos;
+    private Vector3 _mousePos;
 
     [SerializeField] private bool _isAuto;
     [SerializeField] private float _fireDelay;
     private float _startFireTime;
 
-    private bool allowInput = true;
+    private bool _gunEndPoint = true;
+    private bool _allowInput = true;
 
     [SerializeField] private float _bulletSize;
 
     private void OnEnable()
     {
-        allowInput = true;
+        _allowInput = true;
     }
 
     private void Update()
     {
-        if (allowInput && Time.timeScale == 1)
+        if (_allowInput && Time.timeScale == 1)
         {
             Aim();
             ShootCheck();
@@ -39,23 +41,25 @@ public class PlayerWeapon : MonoBehaviour
 
     private void Aim()
     {
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0f;
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _mousePos.z = 0f;
 
-        Vector3 aimDir = (mousePos - transform.position).normalized;
+        Vector3 aimDir = (_mousePos - transform.position).normalized;
         float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
         transform.eulerAngles = new Vector3(0, 0, angle - 90);
 
-        Vector3 aimLocalScale = Vector3.one;
+        /*
+         Vector3 aimLocalScale = Vector3.one;
         if (angle > 90 || angle < -90)
         {
-            aimLocalScale.x = -1f;
+            aimLocalScale.x = 1f;
         }
         else
         {
             aimLocalScale.x = 1f;
         }
         transform.localScale = aimLocalScale;
+        */
     }
 
     private void ShootCheck()
@@ -82,15 +86,27 @@ public class PlayerWeapon : MonoBehaviour
     {
         //AudioManager.PlaySound("PoisonShot");
         
-        gunEndPointPosition = shootTransform.position;
-        
-        Transform bulletTransform = Instantiate(bulletRef.transform, gunEndPointPosition, Quaternion.identity);
-        Vector3 shootDir = (gunEndPointPosition - transform.position).normalized;
+        Vector3 aimPosition = _aimTransform.position;
+        Vector3 gunEndPointPosition1 = _shootTransform1.position;
+        Vector3 gunEndPointPosition2 = _shootTransform2.position;
 
-        Vector3 aimDir = (mousePos - transform.position).normalized;
+        Vector3 aimDir = (_mousePos - transform.position).normalized;
         float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
-
-        bulletTransform.GetComponent<PlayerBullets>().BulletSetup(shootDir, angle, 20, 1, 3, _bulletSize);
+        
+        if (_gunEndPoint)
+        {
+            Transform bulletTransform = Instantiate(_bulletRef.transform, gunEndPointPosition1, Quaternion.identity);
+            Vector3 shootDir = (aimPosition - transform.position).normalized;
+            bulletTransform.GetComponent<PlayerBullets>().BulletSetup(shootDir, angle, 20, 1, 3, _bulletSize);
+        }
+        else
+        {
+            Transform bulletTransform = Instantiate(_bulletRef.transform, gunEndPointPosition2, Quaternion.identity);
+            Vector3 shootDir = (aimPosition - transform.position).normalized;
+            bulletTransform.GetComponent<PlayerBullets>().BulletSetup(shootDir, angle, 20, 1, 3, _bulletSize);
+        }
+        
+        _gunEndPoint = !_gunEndPoint;
 
         // Recoil
         //transform.GetChild(0).position = transform.GetChild(0).position += (-shootDir * 0.75f);
