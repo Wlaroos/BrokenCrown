98314b4a3833ba6228b19b3e63fd50520dbd736c
Mikey <mikeybess27@gmail.com>
2024-09-28 22:31:56 -0500

    Reroller works, added reroll icon on ground

--- Assets/Scripts/Reroller.cs
+++ Assets/Scripts/Reroller.cs
@@ -1,18 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Reroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _rerollPrice = 0.05f;
+    private GameObject _itemTextbox;
+    private TMP_Text _itemName;
+    private TMP_Text _itemDescription;
+    private TMP_Text _itemPrice;
+    private bool _isOverlapping;
+    private TextPopup _textPopupRef;
+
+    private void Awake()
     {
+        _textPopupRef = GameObject.Find("Text Popup").GetComponent<TextPopup>();
+        _itemTextbox = GameObject.Find("Item Textbox");
         
+        _itemName = _itemTextbox.transform.GetChild(0).GetComponent<TMP_Text>();
+        _itemDescription = _itemTextbox.transform.GetChild(1).GetComponent<TMP_Text>();
+        _itemPrice = _itemTextbox.transform.GetChild(2).GetComponent<TMP_Text>();
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && _isOverlapping)
+        {
+            CheckPurchase();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.GetComponent<PlayerMovement>() != null)
+        {
+            _itemTextbox.SetActive(true);
+            _isOverlapping = true;
+            _itemTextbox.transform.position = transform.position + new Vector3(-2, -3f, 0);
+
+            _itemName.text = "- Reroll -";
+            _itemDescription.text = "Don't like the items? I'll swap em out for ya";
+            _itemPrice.text = "$" + _rerollPrice;
+        }
+    }
+     
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            _isOverlapping = false;
+            _itemTextbox.SetActive(false);
+        }
+    }
+    
+    private void CheckPurchase()
+    {
+        if (PlayerStats.Instance.TotalMoney >= _rerollPrice)
+        {
+            Reroll();
+        }
+        else
+        {
+            _textPopupRef.StartFade();
+        }
+    }
+    
+    private void Reroll()
+    {
+        ShopPedestal[] tet = FindObjectsOfType<ShopPedestal>();
+            
+        foreach (var VARIABLE in tet)
+        {
+            VARIABLE.Reroll();
+        }
         
+        PlayerStats.Instance.ChangeMoney(-_rerollPrice);
     }
 }
